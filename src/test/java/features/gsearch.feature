1bc2f45db5fd18f0cd35ff7506dd55e15c15ac9f
seleniummation <seleniummation@yahoo.co.uk>
2017-09-07 23:58:58 +0100

    Modified tag within feature file.

--- src/test/java/features/gsearch.feature
+++ src/test/java/features/gsearch.feature
@@ -1,19 +1,20 @@
 Feature: Google Search
 
-  #@regression
+  @acceptance
   Scenario Outline: Search for multiple locations on google maps
     Given I am on landing page of google maps
     And I enter "<location>" into search text field
     When I click search
     Then Selected locality is displayed as "<expected>"
 
-    @sanity
+    @acceptance
     Examples:
       | location  | expected  |
       | Bristol   | Bristol   |
       | Hereford  | Hereford  |
 
-    @regression
+
+  @regression
     Examples:
       | location  | expected  |
       | Reading   | Reading   |
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -7,16 +7,18 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/java/features"},
 
-        //~ Negation symbol excludes regression tagged scenarios during runtime execution
-        //tags = "~@regression",
+
 
         format = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"},
         glue="steps",
 
-        //AND operator executes scenarios within multiple tags
-        //tags =  {"@sanity", "@regression"}
+         //Negation symbol excludes regression tagged scenarios during runtime execution
+        tags = "~@regression"
 
-        tags =  "@sanity"
+        //AND operator executes scenarios within multiple tags
+        //tags =  {"@acceptance", "~@regression"}
+
+        //tags =  "@sanity"
 
 
 )
